7c9f0c143fa8153da555286aa1b8271e30437a48
Vincent Deng <vincentdeng.it@gmail.com>
2023-08-07 15:25:30 +1000

    test modules output

--- src/services/scan-service.js
+++ src/services/scan-service.js
@@ -54,6 +54,8 @@ async function getModules(vid, vkey, jarName, appId, include) {
     });
   });
 
+  console.log(modules);
+
   const modulesToScan = include.trim().split(',');
   let moduleIds = [];
   modulesToScan.forEach(moduleName => {
--- test/playground.js
+++ test/playground.js
@@ -1,3 +1,17 @@
-// const { minimatch } = require('minimatch')
+const { minimatch } = require('minimatch')
 
 // console.log(minimatch('bar.foo', 'b??.foo'));
+
+const include = 'Antlr3*.dll,Verademo*.dll';
+const modules = [
+  "Antlr3.Runtime.dll"
+]
+const modulesToScan = include.trim().split(',');
+let moduleIds = [];
+modulesToScan.forEach(moduleName => {
+  const module = modules.find(m => minimatch(m.name.toLowerCase(), moduleName.trim().toLowerCase()))
+  if (module) {
+    moduleIds.push(module.id);
+  }
+});
+console.log(moduleIds);
