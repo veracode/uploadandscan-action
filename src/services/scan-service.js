16c82d1ef71ce947718204066507921bd81d2e89
veeranjaneya reddy <vreddy@veracode.com>
2025-01-02 13:45:44 +0530

    removed output console

--- src/services/scan-service.js
+++ src/services/scan-service.js
@@ -67,8 +67,7 @@ async function createSandboxBuild(vid, vkey, jarName, appId, version, deleteinco
   ];
   let output = await runCommand(createBuildCommand, createBuildArguments);
   let outputString = output.toString('utf8'); // Convert buffer to string
-  console.log(outputString);
-  console.log('output', output);
+  console.log('outputString', outputString);
   if (output === 'failed' && deleteincompletescan === 'false') {
     throw new Error(`Error creating build: ${output}`);
   }
